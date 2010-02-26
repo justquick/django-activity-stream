d35673b0ff0d0764e5c4b66c1694a2ae760f8458
Justin Quick <justquick@gmail.com>
2010-02-26 15:54:23 -0500

    activities at base, added comments and accounts

--- example_project/urls.py
+++ example_project/urls.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^activity/', include('actstream.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^comments/', include('django.contrib.comments.urls')),
+    (r'^accounts/', include('registration.backends.default.urls')),
+    (r'', include('actstream.urls')),
 )
