34a65dbaf2ee280d7afc143e7e06bd92ad33a3ab
Aaron Williamson <aaron@copiesofcopies.org>
2010-04-06 22:46:30 -0400

    Made all urls reversible, updated templates to use new names

--- actstream/urls.py
+++ actstream/urls.py
@@ -3,12 +3,12 @@ from django.conf import settings
 from actstream.feeds import *
 
 urlpatterns = patterns('actstream.views',
-    url(r'^feed/(?P<content_type_id>\d+)/(?P<object_id>\d+)/atom/$', AtomObjectActivityFeed()),
-    url(r'^feed/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', ObjectActivityFeed()),
-    url(r'^feed/(?P<content_type_id>\d+)/atom/$', AtomModelActivityFeed()),    
-    url(r'^feed/(?P<content_type_id>\d+)/$', ModelActivityFeed()),
-    url(r'^feed/$', UserActivityFeed()),    
-    url(r'^feed/atom/$', AtomUserActivityFeed()),    
+    url(r'^feed/(?P<content_type_id>\d+)/(?P<object_id>\d+)/atom/$', AtomObjectActivityFeed(), name='actstream_object_feed_atom'),
+    url(r'^feed/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', ObjectActivityFeed(), name='actstream_object_feed'),
+    url(r'^feed/(?P<content_type_id>\d+)/atom/$', AtomModelActivityFeed(), name='actstream_model_feed_atom'),    
+    url(r'^feed/(?P<content_type_id>\d+)/$', ModelActivityFeed(), name='actstream_model_feed'),
+    url(r'^feed/$', UserActivityFeed(), name='actstream_feed'),
+    url(r'^feed/atom/$', AtomUserActivityFeed(), name='actstream_feed_atom'),    
     
     url(r'^follow/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
         'follow', name='actstream_follow'),
