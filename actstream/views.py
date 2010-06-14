74c67c9116b566904e8c863b52f1f5802698c5bc
Justin Quick <justquick@gmail.com>
2010-06-14 02:20:13 -0400

    cleaned up, reorganized, ate some dogfood

--- actstream/views.py
+++ actstream/views.py
@@ -5,30 +5,31 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 
-from actstream.models import Follow, Action, user_stream, actor_stream, model_stream
+from actstream.models import Follow, Action, user_stream, actor_stream, \
+    model_stream, follow, unfollow
+
+def respond(request, code):
+    """
+    Responds to the request with the given response code.
+    If ``next`` is in the form, it will redirect instead
+    """
+    if 'next' in request.REQUEST:
+        return HttpResponseRedirect(request.REQUEST['next'])
+    return type('Response%d' % code, (HttpResponse,), {'status_code': code})()
 
 @login_required
-def follow_unfollow(request, content_type_id, object_id, follow=True):
+def follow_unfollow(request, content_type_id, object_id, do_follow=True):
     """
-    Creates follow relationship st ``request.user`` starts following the actor defined by ``content_type_id``, ``object_id``
+    Creates or deletes the follow relationship between ``request.user`` and the actor defined by ``content_type_id``, ``object_id``
     """
     ctype = get_object_or_404(ContentType, pk=content_type_id)
     actor = get_object_or_404(ctype.model_class(), pk=object_id)
-    lookup = {
-        'user': request.user,
-        'content_type': ctype,
-        'object_id': object_id,
-    }
-    def resp(code=201):
-        if 'next' in request.REQUEST:
-            return HttpResponseRedirect(request.REQUEST['next'])
-        return type('Response%d' % code, (HttpResponse,), {'status_code': code})()
         
-    if follow:
-        Follow.objects.get_or_create(**lookup)
-        return resp()
-    Follow.objects.get(**lookup).delete()
-    return resp(204)
+    if do_follow:
+        follow(request.user, actor)
+        return respond(request, 201) # CREATED
+    unfollow(request.user, actor)
+    return respond(request, 204) # NO CONTENT
     
 @login_required
 def stream(request):
@@ -48,14 +49,14 @@ def followers(request, content_type_id, object_id):
     follows = Follow.objects.filter(content_type=ctype, object_id=object_id)
     actor = get_object_or_404(ctype.model_class(), pk=object_id)
     return render_to_response('activity/followers.html', {
-        'followers': [f.user for f in follows], 'actor':actor
+        'followers': (f.user for f in follows), 'actor':actor
     }, context_instance=RequestContext(request))
     
 def user(request, username):
     """
     ``User`` focused activity stream. (Eg: Profile page twitter.com/justquick)
     """
-    user = get_object_or_404(User, username=username)
+    user = get_object_or_404(User, username=username, is_active=True)
     return render_to_response('activity/actor.html', {
         'ctype': ContentType.objects.get_for_model(User),
         'actor':user,'action_list':actor_stream(user)
