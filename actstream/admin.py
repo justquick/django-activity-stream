05750642efde00069c024090054c73cb5a57df7a
Justin Quick <justquick@gmail.com>
2012-02-22 22:50:43 -0500

    raw id fields save lives

--- actstream/admin.py
+++ actstream/admin.py
@@ -7,12 +7,15 @@ class ActionAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'actor', 'verb', 'target')
     list_editable = ('verb',)
     list_filter = ('timestamp',)
+    raw_id_fields = ('actor_content_type','target_content_type',
+                     'action_object_content_type')
 
 
 class FollowAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'user', 'actor')
     list_editable = ('user',)
     list_filter = ('user',)
+    raw_id_fields = ('user', 'content_type')
 
 
 admin.site.register(models.Action, ActionAdmin)
