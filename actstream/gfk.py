d48c8ca8de34765b714025ad3fdc6fcb83ef0364
Ben Slavin <benjamin.slavin@gmail.com>
2012-04-15 11:38:51 -0400

    Ignoring corrupted GFK values (one of CType or ID is None)

--- actstream/gfk.py
+++ actstream/gfk.py
@@ -54,6 +54,7 @@ class GFKQuerySet(QuerySet):
             for gfk in gfk_fields:
                 ct_id_field = self.model._meta.get_field(gfk.ct_field).column
                 if getattr(item, ct_id_field) is None: continue
+                if getattr(item, gfk.fk_field) is None: continue
                 ct_map.setdefault(getattr(item, ct_id_field), {}
                     )[smart_unicode(getattr(item, gfk.fk_field))] = (gfk.name,
                         item.pk)
