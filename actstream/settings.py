ef554c3b3623d5be691d7547c0c78d37ddb5702f
Ben Slavin <benjamin.slavin@gmail.com>
2012-04-15 13:08:55 -0400

    Adding class_prepared listener for population of actstream.settings.MODELS when models are not available to get_model at import time.

--- actstream/settings.py
+++ actstream/settings.py
@@ -1,8 +1,26 @@
 from django.conf import settings
 from django.db.models import get_model
+from django.db.models.signals import class_prepared
+
+ACTSTREAM_ACTION_MODELS = [m.lower() for m in getattr(settings,
+    'ACTSTREAM_ACTION_MODELS', ('auth.User',))]
 
 MODELS = {}
-for model in getattr(settings, 'ACTSTREAM_ACTION_MODELS', ('auth.User',)):
+
+# Certain application configurations can lead to models not being available
+# to get_model at import time. To overcome this problem, we register a
+# class_prepared listener. This needs to be done before we do the initial
+# population of MODELS, since we cannot predict what is already available,
+# what will be available later, and what will be discovered during model
+# loading.
+def late_registration(sender, **kwargs):
+    opts = sender._meta
+    key = "%s.%s" % (opts.app_label,opts.module_name)
+    if key in ACTSTREAM_ACTION_MODELS:
+        MODELS[key] = sender
+class_prepared.connect(late_registration)
+
+for model in ACTSTREAM_ACTION_MODELS:
     MODELS[model.lower()] = model = get_model(*model.split('.'))
 
 MANAGER_MODULE = getattr(settings, 'ACTSTREAM_MANAGER',
