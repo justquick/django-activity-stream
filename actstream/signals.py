409c26d94ce45759baccca816af48320e7d87766
Justin Quick <justquick@gmail.com>
2013-06-08 16:47:12 -0400

    dropping actor signal args as it uses sender instead fixes #126

--- actstream/signals.py
+++ actstream/signals.py
@@ -1,4 +1,4 @@
 from django.dispatch import Signal
 
-action = Signal(providing_args=['actor', 'verb', 'action_object', 'target',
-    'description', 'timestamp'])
+action = Signal(providing_args=['verb', 'action_object', 'target',
+                                'description', 'timestamp'])
