adcd85788d5fab83132ce820f94b7b440d85be00
Justin Quick <justquick@gmail.com>
2013-06-29 05:36:11 -0400

    streamlining runtests, realistic tox.ini

--- actstream/runtests/runtests.py
+++ actstream/runtests/runtests.py
@@ -11,39 +11,9 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'actstream.runtests.settings'
 
-
-import django
 from django.conf import settings
 from django.test.utils import get_runner
 
 
-def usage():
-    return """
-    Usage: python runtests.py [UnitTestClass].[method]
-
-    You can pass the Class name of the `UnitTestClass` you want to test.
-
-    Append a method name if you only want to test a specific method of that class.
-    """
-
-
-def main():
-    TestRunner = get_runner(settings)
-
-    test_runner = TestRunner()
-    if len(sys.argv) == 2:
-        test_case = '.' + sys.argv[1]
-    elif len(sys.argv) == 1:
-        test_case = ''
-    else:
-        print(usage())
-        sys.exit(1)
-    test_module_name = 'actstream'
-    #if django.VERSION[0] == 1 and django.VERSION[1] < 6:
-    #    test_module_name = 'tests'
-    failures = test_runner.run_tests([test_module_name + test_case])
-
-    sys.exit(failures)
-
 if __name__ == '__main__':
-    main()
+    sys.exit(get_runner(settings)().run_tests(['actstream', 'testapp']))
