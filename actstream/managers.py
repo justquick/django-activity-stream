312172a14af393552267e7ea6f1ee5a99037a363
Thomas Khyn <thomas@ksytek.com>
2014-04-23 14:59:34 +1200

    Added verbs support for the ActionManager's user method
    --HG--
    branch : verb_sorting

--- actstream/managers.py
+++ actstream/managers.py
@@ -65,34 +65,58 @@ class ActionManager(GFKManager):
         """
         q = Q()
         qs = self.filter(public=True)
-        actors_by_content_type = defaultdict(lambda: [])
-        others_by_content_type = defaultdict(lambda: [])
+        actors_by_content_type = defaultdict(lambda: defaultdict(lambda: []))
+        others_by_content_type = defaultdict(lambda: defaultdict(lambda: []))
 
         follow_gfks = get_model('actstream', 'follow').objects.filter(
-            user=object).values_list('content_type_id',
-                                     'object_id', 'actor_only')
+            user=object).values_list('content_type_id', 'object_id',
+                                     'verbs', 'actor_only')
 
         if not follow_gfks:
             return qs.none()
 
-        for content_type_id, object_id, actor_only in follow_gfks.iterator():
-            actors_by_content_type[content_type_id].append(object_id)
-            if not actor_only:
-                others_by_content_type[content_type_id].append(object_id)
+        for content_type_id, object_id, verbs, actor_only \
+        in follow_gfks.iterator():
+            abct = actors_by_content_type[content_type_id]
+            if abct[object_id] is None:
+                # the object_id is already marked to 'use' all verbs
+                continue
+            obct = others_by_content_type[content_type_id]
+            if verbs:
+                verbs = verbs.split(';')
+                # append the verb
+                abct[object_id].extend(verbs)
+                if not actor_only:
+                    obct[object_id].extend(verbs)
+            else:
+                # no verb specified, mark it as None, which means 'all verbs'
+                abct[object_id] = None
+                if not actor_only:
+                    obct[object_id] = None
 
-        for content_type_id, object_ids in actors_by_content_type.iteritems():
-            q = q | Q(
-                actor_content_type=content_type_id,
-                actor_object_id__in=object_ids,
-            )
-        for content_type_id, object_ids in others_by_content_type.iteritems():
-            q = q | Q(
-                target_content_type=content_type_id,
-                target_object_id__in=object_ids,
-            ) | Q(
-                action_object_content_type=content_type_id,
-                action_object_object_id__in=object_ids,
-            )
+        for content_type_id, objects in actors_by_content_type.iteritems():
+            for o_id, verbs in objects.iteritems():
+                kws = dict(
+                    actor_content_type=content_type_id,
+                    actor_object_id=o_id,
+                )
+                if verbs:
+                    kws['verb__in'] = verbs
+                q = q | Q(**kws)
+        for content_type_id, objects in others_by_content_type.iteritems():
+            for o_id, verbs in objects.iteritems():
+                kws_target = dict(
+                    target_content_type=content_type_id,
+                    target_object_id=o_id,
+                )
+                kws_actobj = dict(
+                    action_object_content_type=content_type_id,
+                    action_object_object_id=o_id,
+                )
+                if verbs:
+                    kws_target['verb__in'] = \
+                    kws_actobj['verb__in'] = verbs
+                q = q | Q(**kws_target) | Q(**kws_actobj)
         qs = qs.filter(q, **kwargs)
         return qs
 
