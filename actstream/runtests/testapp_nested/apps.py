acaf7dff317ef3f5fc8d1d257ebcb6ded5d7d3a7
Justin Quick <justquick@gmail.com>
2014-08-17 05:34:52 -0400

    testapp_nested appconfig

---
+++ actstream/runtests/testapp_nested/apps.py
@@ -0,0 +1,12 @@
+try:
+    from django.apps import AppConfig
+except ImportError:
+    AppConfig = object
+
+
+class TestappNestedConfig(AppConfig):
+    name = 'actstream.runtests.testapp_nested'
+
+    def ready(self):
+        from actstream.registry import register
+        register(self.get_model('nestedmodel'))
