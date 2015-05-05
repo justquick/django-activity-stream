31094f3920dbfe0dd5febec1e8289921906c90b6
Shanto <shanto@hotmail.com>
2015-05-06 01:00:40 +0600

    Replace problamatic CharField GFKs with PositiveInterField fields. Using CharField for GFK's PK part produces SQL errors on JOIN (in PostgreSQL).

--- actstream/models.py
+++ actstream/models.py
@@ -29,7 +29,7 @@ class Follow(models.Model):
     user = models.ForeignKey(user_model_label, db_index=True)
 
     content_type = models.ForeignKey(ContentType, db_index=True)
-    object_id = models.CharField(max_length=255, db_index=True)
+    object_id = models.PositiveIntegerField(db_index=True)
     follow_object = generic.GenericForeignKey()
     actor_only = models.BooleanField("Only follow actions where "
                                      "the object is the target.", default=True)
@@ -75,7 +75,7 @@ class Action(models.Model):
     """
     actor_content_type = models.ForeignKey(ContentType, related_name='actor',
                                            db_index=True)
-    actor_object_id = models.CharField(max_length=255, db_index=True)
+    actor_object_id = models.PositiveIntegerField(db_index=True)
     actor = generic.GenericForeignKey('actor_content_type', 'actor_object_id')
 
     verb = models.CharField(max_length=255, db_index=True)
@@ -83,13 +83,13 @@ class Action(models.Model):
 
     target_content_type = models.ForeignKey(ContentType, blank=True, null=True,
                                             related_name='target', db_index=True)
-    target_object_id = models.CharField(max_length=255, blank=True, null=True, db_index=True)
+    target_object_id = models.PositiveIntegerField(blank=True, null=True, db_index=True)
     target = generic.GenericForeignKey('target_content_type',
                                        'target_object_id')
 
     action_object_content_type = models.ForeignKey(ContentType, blank=True, null=True,
                                                    related_name='action_object', db_index=True)
-    action_object_object_id = models.CharField(max_length=255, blank=True, null=True, db_index=True)
+    action_object_object_id = models.PositiveIntegerField(blank=True, null=True, db_index=True)
     action_object = generic.GenericForeignKey('action_object_content_type',
                                               'action_object_object_id')
 
