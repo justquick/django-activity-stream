a3895893114c2eb01029966dc680685bc02f9e8b
Justin Quick <justquick@gmail.com>
2016-02-10 22:29:38 -0500

    better db debugging, passenv for TRAVIS var

--- actstream/runtests/settings.py
+++ actstream/runtests/settings.py
@@ -37,7 +37,7 @@ if 'postgres' in ENGINE or 'mysql' in ENGINE:
         HOST=os.environ.get('DATABASE_HOST', 'localhost')
     )
 
-print(ENGINE)
+print(DATABASES)
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
