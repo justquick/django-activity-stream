321587fad77d1907efc9c2084953d6ffdf23db1a
Justin Quick <justquick@gmail.com>
2016-05-07 16:29:09 -0400

    start of postgres jsonb work. test are broken

--- actstream/compat.py
+++ actstream/compat.py
@@ -8,6 +8,11 @@ user_model_label = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 username_field = lambda: getattr(get_user_model(), 'USERNAME_FIELD', 'username')
 
 try:
+    is_postgres = 'postgres' in settings.DATABASES['default']['ENGINE']
+except KeyError:
+    is_postgres = False
+
+try:
     from django.contrib.auth import get_user_model
 except ImportError:
     from django.contrib.auth.models import User
@@ -29,3 +34,16 @@ try:
     get_model = apps.get_model
 except ImportError:
     from django.db.models import get_model
+
+JSONField = None
+if is_postgres:
+    try:
+        # Django >= 1.8 has native Postgres jsonb support
+        from django.contrib.postgres.fields import JSONField
+    except ImportError:
+        pass
+if JSONField is None:
+    try:
+        from jsonfield.fields import JSONField
+    except ImportError:
+        pass
