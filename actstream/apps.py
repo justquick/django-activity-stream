0cbfa8a1df5cbf21ace918168a204952e7a762be
Steve Lacey <steve@stevelacey.net>
2016-08-29 01:35:42 +0200

    Skip patching in the data field, it's a user land problem now

--- actstream/apps.py
+++ actstream/apps.py
@@ -11,12 +11,3 @@ class ActstreamConfig(AppConfig):
     def ready(self):
         from actstream.actions import action_handler
         action.connect(action_handler, dispatch_uid='actstream.models')
-        action_class = self.get_model('action')
-
-        if settings.USE_JSONFIELD:
-            try:
-                from jsonfield.fields import JSONField
-            except ImportError:
-                raise ImproperlyConfigured('You must have django-jsonfield installed '
-                                           'if you wish to use a JSONField on your actions')
-            JSONField(blank=True, null=True).contribute_to_class(action_class, 'data')
