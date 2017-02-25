5e20129e711bebe01afb8d51a3d4d11f494f2d29
cmwaura <cmmwaura@ucdavis.edu>
2017-02-24 20:54:51 -0800

    removed imported django module

--- actstream/views.py
+++ actstream/views.py
@@ -1,4 +1,3 @@
-import django
 from django.shortcuts import render_to_response, get_object_or_404, render
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
