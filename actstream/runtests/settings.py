1cd38f2a0bf512418ff43d53112acfad5ffb0e64
Justin Quick <justquick@gmail.com>
2017-03-26 21:36:09 -0400

    test fixes

--- actstream/runtests/settings.py
+++ actstream/runtests/settings.py
@@ -86,6 +86,16 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     'django.core.context_processors.request',
 ]
 
+if django.VERSION >= (1, 10):
+    TEMPLATE_CONTEXT_PROCESSORS = [
+        'django.contrib.auth.context_processors.auth',
+        'django.template.context_processors.debug',
+        'django.template.context_processors.i18n',
+        'django.template.context_processors.media',
+        'django.template.context_processors.static',
+        'django.template.context_processors.tz',
+    ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
