8fdc38472bd969cc992a2677d1a5e2e34a26c390
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-03-09 11:11:25 +0600

    middleware config to latest version

--- actstream/runtests/settings.py
+++ actstream/runtests/settings.py
@@ -107,7 +107,7 @@ TEMPLATES = [
     },
 ]
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
