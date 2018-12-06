2da1ea8e31023b58f84ed9f8091e91bfcc81e86b
Christoph Buelter <christoph.buelter@myshopradio.de>
2018-12-06 14:33:35 +0100

    Fix second import not propery wrapped in try/except block.

--- actstream/jsonfield.py
+++ actstream/jsonfield.py
@@ -1,4 +1,4 @@
-"""
+'''
 
 Decide on a JSONField implementation based on available packages.
 
@@ -10,7 +10,7 @@ There are three possible options, preferred in the following order:
 Raises an ImportError if USE_JSONFIELD is True but none of these are
 installed. Falls back to a simple TextField if USE_JSONFIELD is False.
 
-"""
+'''
 
 from django.db import models
 from django.core.exceptions import ImproperlyConfigured
@@ -18,24 +18,26 @@ from django.core.exceptions import ImproperlyConfigured
 from actstream.settings import USE_JSONFIELD
 
 
-__all__ = ("DataField", "JSONField", "register_app")
+__all__ = ('DataField', 'JSONField', 'register_app')
 
 
 if USE_JSONFIELD:
     try:
         from jsonfield_compat import JSONField, register_app
-    except ImportError:
-        from django_mysql.models import JSONField
+    except ImportError as err:
+        try:
+            from django_mysql.models import JSONField
 
-        def register_app(app):
-            pass
+            def register_app(app):
+                pass
 
-    except ImportError:
-        raise ImproperlyConfigured(
-            'You must either have django-jsonfield and django-jsonfield-compat'
-            ' or django-mysql installed if you wish to use a JSONField on your'
-            ' actions'
-        )
+        except ImportError:
+            raise ImproperlyConfigured(
+                'You must either install django-jsonfield + '
+                'django-jsonfield-compat, or django-mysql as an '
+                'alternative, if you wish to use a JSONField on your '
+                'actions'
+            )
     DataField = JSONField
 else:
     DataField = models.TextField
