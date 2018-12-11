2388e0956345639ef15d2b6eb0c7bd273fc39505
Christoph Bülter <cb109@users.noreply.github.com>
2018-12-11 16:57:47 +0100

    Add support for django-mysql's JSONField as alternative implementation (#417)
    * Move JSONField setup to a separate module and support django-mysql on top.
    
    * Fix second import not propery wrapped in try/except block.
    
    * Add a debug statement to check which JSONField was chosen.
    
    * Fix py37 not installing mysqslclient during testruns.
    
    * Install either djangojsonfield or django-mysql depending on tox env.
    
    Also simplify the env setup, so that there is a little less redundancy.
    
    * Check for mysql version (must be 5.7+) before using django-mysql.
    
    * Revert mysql version check in code, as it would need a db connection.
    
    Would otherwise have to rely on some specific 3rd party lib like MySQLdb
    or pymysql, which is also no a great idea.
    
    * Make sure the mysql version used for tests is 5.7 to support django-mysql.
    
    * Remove empty line.
    
    * Add mysql version remark.
    
    * Update docs about supported JSONField implementations.
    
    * Fix ambiguity.
    
    * Update docstring about TextField being only a temporary fallback.
    
    * Update changelog.

--- actstream/apps.py
+++ actstream/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.core.exceptions import ImproperlyConfigured
 
 from actstream import settings
 from actstream.signals import action
@@ -14,12 +13,8 @@ class ActstreamConfig(AppConfig):
         action_class = self.get_model('action')
 
         if settings.USE_JSONFIELD:
-            try:
-                from jsonfield_compat import JSONField, register_app
-            except ImportError:
-                raise ImproperlyConfigured(
-                    'You must have django-jsonfield and django-jsonfield-compat '
-                    'installed if you wish to use a JSONField on your actions'
-                )
-            JSONField(blank=True, null=True).contribute_to_class(action_class, 'data')
+            from actstream.jsonfield import DataField, register_app
+            DataField(blank=True, null=True).contribute_to_class(
+                action_class, 'data'
+            )
             register_app(self)
