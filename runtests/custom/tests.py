0e6fe879b2184518ed9bee5796845c37c6495bda
Justin Quick <justquick@gmail.com>
2021-03-23 21:26:52 -0400

    wip: custom test app

---
+++ runtests/custom/tests.py
@@ -0,0 +1,29 @@
+from django.test.utils import override_settings
+
+from actstream.settings import get_action_model, get_follow_model
+from actstream.signals import action
+from actstream.tests.base import ActivityBaseTestCase
+
+from .models import CustomAction, CustomFollow
+
+
+@override_settings(
+    ACTSTREAM_ACTION_MODEL='custom.CustomAction',
+    ACTSTREAM_FOLLOW_MODEL='custom.CustomFollow'
+)
+class CustomModelTests(ActivityBaseTestCase):
+    def setUp(self):
+        super(CustomModelTests, self).setUp()
+        self.user = self.User.objects.create(username='test')
+
+    def test_custom_action_model(self):
+        self.assertEqual(get_action_model(), CustomAction)
+
+    def test_custom_follow_model(self):
+        self.assertEqual(get_follow_model(), CustomFollow)
+
+    def test_custom_data(self):
+        """Adding custom data to a model field works as expected."""
+        action.send(self.user, verb='was created', quest='to be awesome')
+        self.assertEqual(CustomAction.objects.count(), 1)
+        self.assertEqual(CustomAction.objects.first().quest, 'to be awesome')
