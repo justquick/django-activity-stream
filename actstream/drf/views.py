253b93a25038221be30d6728afb887ff99ee8489
Justin Quick <justquick@gmail.com>
2022-02-27 21:08:20 -0500

    allow a 'my actions' view as viewset action

--- actstream/drf/views.py
+++ actstream/drf/views.py
@@ -1,27 +1,39 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 from actstream.drf.serializers import FollowSerializer, ActionSerializer, registered_serializers, registry_factory
-from actstream.models import Action, Follow
+from actstream.models import Action, Follow, actor_stream
 from actstream.registry import label
 from actstream.settings import DRF_SETTINGS, import_obj
 
 
 class DefaultModelViewSet(viewsets.ReadOnlyModelViewSet):
     def get_permissions(self):
-        print(DRF_SETTINGS)
         return [import_obj(permission)() for permission in DRF_SETTINGS['PERMISSIONS']]
 
 
 class ActionViewSet(DefaultModelViewSet):
     queryset = Action.objects.public().prefetch_related()
     serializer_class = ActionSerializer
-    # permission_classes = [permissions.IsAuthenticated]
 
     @action(detail=False, methods=['POST'])
     def send(self, request):
         pass
+
+    def get_stream(self, stream):
+        page = self.paginate_queryset(stream)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(stream, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, permission_classes=[permissions.IsAuthenticated], name='My Actions')
+    def me(self, request):
+        return self.get_stream(actor_stream(request.user))
 
 
 class FollowViewSet(DefaultModelViewSet):
@@ -48,7 +60,6 @@ def viewset_factory(model_class, queryset=None):
     return type(f'{model_class.__name__}ViewSet', (DefaultModelViewSet,), {
         'queryset': queryset,
         'serializer_class': serializer_class,
-        # 'permission_classes': [permissions.IsAuthenticated]
     })
 
 
