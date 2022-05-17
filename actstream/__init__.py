49749403321d16f14ecf0f6f95d5511e5429d7a2
Lutaaya Idris <lutaaya.idris@captiq.com>
2022-05-17 16:15:05 +0300

    Fix django app config default

--- actstream/__init__.py
+++ actstream/__init__.py
@@ -8,5 +8,8 @@ import django
 __version__ = '1.4.0'
 __author__ = 'Asif Saif Uddin, Justin Quick <justquick@gmail.com>'
 
-if django.VERSION < (3, 2):
+if django.VERSION >= (3, 2):
+    # The declaration is only needed for older Django versions
+    pass
+else:
     default_app_config = 'actstream.apps.ActstreamConfig'
