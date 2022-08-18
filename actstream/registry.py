7a5f3e82264dcde82aa0d8c3a16f1446f8634051
Justin Quick <justquick@gmail.com>
2022-08-17 20:55:09 -0400

    django 4.1 in tox

--- actstream/registry.py
+++ actstream/registry.py
@@ -55,9 +55,9 @@ def label(model_class):
 def is_installed(model_class):
     """
     Returns True if a model_class is installed.
-    model_class._meta.installed is only reliable in Django 1.7+
+    model_class._meta.app_config is only reliable in Django 1.7+
     """
-    return model_class._meta.installed
+    return model_class._meta.app_config is not None
 
 
 def validate(model_class, exception_class=ImproperlyConfigured):
